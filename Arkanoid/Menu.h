666a295d70cb356e7c23ec13adf84a1bb9a8d51b
BIG_PANDA <viens.louc@hotmail.com>
2024-05-16 09:46:58 -0400

    new menu

--- Arkanoid/Menu.h
+++ Arkanoid/Menu.h
@@ -1,13 +1,24 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Font.hpp>
 class Menu
 {
 private:
 	sf::RectangleShape _titre;
 	sf::Texture _textureTitre;
+
+	sf::Text _play;
+	sf::Text _exit;
+	sf::Font font;
+	int option;
 public:
 	Menu();
 	bool setTextureTitre();
+	bool setTitre();
 	void drawTitre(sf::RenderWindow& window);
+	void drawOption(sf::RenderWindow& window);
+	int switchMenu(sf::Event& event);
+	void defaultOption(sf::RenderWindow& window);
+	void confirmOption(sf::RenderWindow& window, int option);
 };
 
